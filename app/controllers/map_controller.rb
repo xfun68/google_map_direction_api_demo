46688f4f1a204b9e6e93affd953c69b6f4766759
xfun68 <xfun68@gmail.com>
2011-06-30 17:53:57 +0800

    Fixed a bug about wrong hash key. pair:xmyang

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -8,7 +8,7 @@ class MapController < ApplicationController
     @has_search_result = false
     return if params[:map].nil?
 
-    json_result = search_routes(params["start_address"], params["end_address"])
+    json_result = search_routes(params["map"]["start_address"], params["map"]["end_address"])
     @search_result = ActiveSupport::JSON.decode(json_result)
     @has_search_result = true if @search_result["status"]  == "OK"
   end
